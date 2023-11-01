5a8c6fcb64f65788477db9e1c3b33948fb56aec6
Shailendra Ahir <shailendra@shailahir.com>
2023-11-01 14:01:01 +0530

    Updated posts
    Signed-off-by: Shailendra Ahir <shailendra@shailahir.com>

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,20 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Backstage.io 101',
+  author: 'Shailendra ahir',
+  headerTitle: 'BackstageIO101',
+  description: 'Tutorials and articles on Spotify Backstage',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://backstageio101.com',
+  siteRepo: 'https://github.com/shailahir/myblog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'shailendra@shailahir.com',
+  github: 'https://github.com/shailahir',
+  twitter: 'https://twitter.com/shail_ahir',
+  linkedin: 'https://www.linkedin.com/in/shailendra-ahir/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
