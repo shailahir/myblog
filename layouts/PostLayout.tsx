7431c444e192241740768d12439cf0e7d4101243
Shailendra Ahir <shailendra@shailahir.com>
2023-11-09 13:00:02 +0530

    Updated img tag
    Signed-off-by: Shailendra Ahir <shailendra@shailahir.com>

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -74,7 +74,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
+                        {/* <dt className="sr-only">Twitter</dt>
                         <dd>
                           {author.twitter && (
                             <Link
@@ -84,7 +84,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                               {author.twitter.replace('https://twitter.com/', '@')}
                             </Link>
                           )}
-                        </dd>
+                        </dd> */}
                       </dl>
                     </li>
                   ))}
